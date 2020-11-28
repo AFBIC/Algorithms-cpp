73be5898b1138c85249137f190b8c412947ce3be
Shane (Treasure) Xue <qq2198631773@outlook.com>
2020-11-28 18:09:50 +0800

    Update stack.hpp
    Added functions

--- stack.hpp
+++ stack.hpp
@@ -1,4 +1,5 @@
 #include <stdexept>
+#include <new>
 #include <cstdlib>
 
 namespace AFBIC{
@@ -7,22 +8,89 @@ namespace AFBIC{
     private:
         T* inptr;
         int size,toploc;
+        //toploc:highest empty location
     public:
-        Stack();
-        explicit Stack(const int size);
-        explicit Stack(Stack s);
+        explicit Stack(const int s);
+        explicit Stack(const Stack s);
+        explicit Stack(const Stack&& s);
         
         ~Stack();
         
-        push();
-        pop();
-        size();
-        top();
-        empty();
-        expand(int new_size);
-        shrink(int new_size);
-        shrink_safe(int new_size);
-        shrink_to_fit();
+        void operator=(Stack s);
+        
+        void push(T data);
+        void pop();
+        int size();
+        T top();
+        bool empty();
         }//Stack
     }//namespace AFBIC
 
+AFBIC::Stack::Stack(const int s){
+    try{
+        inptr=new T[s];
+    }
+    catch(std::bad_alloc& ba){
+        size=0;
+        toploc=0;
+        inptr=nullptr;
+        throw;
+    }
+    size=s;
+    toploc=0;
+}
+
+AFBIC::Stack::Stack(const Stack s){
+    try{
+        inptr=new T[s.size];
+    }
+    catch(std::bad_alloc& ba){
+        size=0;
+        toploc=0;
+        inptr=nullptr;
+        throw;
+    }
+    size=s.size;
+    toploc=s.toploc;
+    for(int head=0;head!=toploc;head++){
+        inptr[head]=s.inptr[head];
+    }
+}
+
+AFBIC::Stack::Stack(const Stack&& s){
+    inptr=s.inptr; //just moving
+    size=s.size;
+    toploc=s.toploc;
+}
+
+AFBIC::Stack::~Stack(){
+    delete(inptr);
+    size=0;
+    toploc=0;
+}
+
+bool AFBIC::Stack::empty(){
+    if(toploc==0) return true;
+    return false;
+}
+
+int AFBIC::Stack::size(){
+    return toploc;
+}
+
+template <typename T>
+T AFBIC::Stack::top(){
+    return inptr[toploc-1];
+}
+
+void AFBIC::Stack::pop(){
+    if(empty()) throw std::runtime_error("Stack Underflow");
+    else toploc=toploc-1;
+}
+
+template <typename T>
+void AFBIC::Stack::push(T data){
+    if(toploc>size) throw std::runtime_error("Stack Overflow");
+    inptr[toploc]=data;
+    toploc++;
+}
